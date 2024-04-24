0cf619863e6f8fb43c6fb8bf8fd350d410df378e
yoonjookim <yjblossom@gmail.com>
2024-04-24 11:58:03 -0400

    Update the number of plants in Header

--- Plant_watering_app/src/components/Header.jsx
+++ Plant_watering_app/src/components/Header.jsx
@@ -1,8 +1,11 @@
+import { plantsArr } from "../data/plantData";
+
+
 export default function Header(props) {
     return (
         <header className="Header">
             <h1>Botanical Concierge with Green-Guardian</h1>
-            <h2>Helping X plants from dehydration</h2>
+            <h2>Helping {plantsArr.length} plants from dehydration</h2>
         </header>
     )
-} 
+}  
