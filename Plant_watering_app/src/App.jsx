cfd5e13fbfeca3cb917bae3c4821f301107e68ca
yoonjookim <yjblossom@gmail.com>
2024-04-25 13:20:04 -0400

    Create buttons <section> to update watered Date as of today

--- Plant_watering_app/src/App.jsx
+++ Plant_watering_app/src/App.jsx
@@ -12,20 +12,46 @@ import { plantsArr } from "./data/plantData";
 
 function App() {
 
-  const [plants, setPlants] = useState(plantsArr)     
+  const [plants, setPlants] = useState(plantsArr);     
   //Hey, let's have a 'state' called 'plants' and the way to change it will be called 'setPlants'.
   //And I'll use 'useState' hook.And the original data will be 'plantsArr' that we have. 
   //Finally, I can control my data from the webpage's rightclick: 'inspect' -> components'
   //I can update the watered date on there, it will be applicable & updated on the page real time.
   
+
+  //below: If I want to update the specific plant's watered date with function called 'updateWateredDate'
+  const updateWateredDate = (plantId) => {   //I need to know plantId
+    const plantToUpdate = plants.find((plant) => plant.id === plantId) //Hey,plants, can you find the plant where the ID matches the plantId of my function?
+    //above, we can use 'find'method on data of 'array' : plantsArr
+    plantToUpdate.lastWatered = new Date().toLocaleDateString() 
+    //Hey,plantToUpdate, you know your lastWatered date right? I want to change it to today.
+    setPlants(plants) //Once we have that, I want to setPlants to plants. Because I mutated the data inside it. 
+  }
+
   return (
     <>
       <Header amount={plants.length}/>   
+    <section>
+      <button onClick={() => updateWateredDate("1")}>Plant 1</button> 
+      <button onClick={() => updateWateredDate("2")}>Plant 2</button>
+      <button onClick={() => updateWateredDate("3")}>Plant 3</button>
+      <button onClick={() => updateWateredDate("4")}>Plant 4</button>
+      <button onClick={() => updateWateredDate("5")}>Plant 5</button>
+      <button onClick={() => updateWateredDate("6")}>Plant 6</button>
+      <button onClick={() => updateWateredDate("7")}>Plant 7</button>
+      <button onClick={() => updateWateredDate("8")}>Plant 8</button>
+      <button onClick={() => updateWateredDate("9")}>Plant 9</button>
+      <button onClick={() => updateWateredDate("10")}>Plant 10</button>
+    </section> 
     <main>
       <PlantList plants={plants}/>
     </main>
     </>
-  )
+  )//Above, in the button,
+  //"1" we put the string with number, because this is what we're dealing with in the Id.
+  //{() => updateWateredDate("1")} The reason we wrapped inside anonymous function is...
+  //because we need to update wateredDate when I click,
+  //not to want to do the result of it. It's like a normal callback. 
 }
 
 export default App
--- Plant_watering_app/src/components/Header.jsx
+++ Plant_watering_app/src/components/Header.jsx
@@ -2,8 +2,8 @@ export default function Header(props) {
     const {amount} = props //I receive a props called amount
     return (
         <header className="Header">
-            <h1>Botanical Concierge with Green-Guardian</h1>
-            <h2>Helping {amount} plants from dehydration</h2>
+            <h1>Botanical Concierge</h1>
+            <h2>Green-guardian helping {amount} plants from dehydration</h2>
         </header>
     )
 }  
